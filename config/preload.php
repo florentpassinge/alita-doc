f4844b62d85df12bff04f9ff0d3d7261b417ba21
Florent Passinge <florent.passinge@gmail.com>
2021-01-31 17:12:35 +0100

    chore: init commit

--- config/preload.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if (file_exists(dirname(__DIR__).'/var/cache/prod/AlitaDoc_KernelProdContainer.preload.php')) {
-    require dirname(__DIR__).'/var/cache/prod/AlitaDoc_KernelProdContainer.preload.php';
-}
